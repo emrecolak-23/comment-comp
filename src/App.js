b2e706d3a370bb83366a56c4ca7ad8f13162be62
Emre Çolak <emrecolak@Emre.home>
2023-01-23 21:18:04 +0300

    add approval card

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
+import './App.css';
 import CommentDetail from './CommentDetail';
-
+import ApprovalCard from './ApprovalCard';
 import { faker } from '@faker-js/faker';
 
 function App() {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 02:00AM"
-        comment="Nice Job"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Jane"
-        timeAgo="Today at 05:00PM"
-        comment="That awesome"
-        image={faker.image.avatar()}
-      />
-      <CommentDetail
-        author="Alex"
-        timeAgo="Yesterday at 09:00AM"
-        comment="Not my business"
-        image={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <h3>Warning!</h3>
+        <div>Are you sure?</div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 02:00AM"
+          comment="Nice Job"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Today at 05:00PM"
+          comment="That awesome"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Yesterday at 09:00AM"
+          comment="Not my business"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 }
